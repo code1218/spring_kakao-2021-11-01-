613611f134e3b6233a0a383755e88f46100c5158
junil <skjil1218@gmail.com>
2021-11-01 17:42:23 +0900

    비밀번호확인 작업중

--- kakao_spring_junil/src/main/webapp/WEB-INF/views/signUp/sign_up.jsp
+++ kakao_spring_junil/src/main/webapp/WEB-INF/views/signUp/sign_up.jsp
@@ -31,7 +31,7 @@
                     </div>
                     <div class="item_msg">
                     	<span class="msg1">필수 항목입니다.</span>
-                    	<span class="msg2">${id }(은)는 이미 존재하는 아이디입니다.</span>
+                    	<span class="msg2"><span id="false_email"></span>(은)는 이미 존재하는 아이디입니다.</span>
                     </div>
                     <div class="confirm_btn">
                         <button type="button" class="btn_g">다음</button>
@@ -53,6 +53,28 @@
                     <div class="confirm_btn">
                         <button type="button" class="btn_g">다음</button>
                     </div>
+                </div>
+                <div class="warp_form">
+                    <form action="signUp" method="post">
+                    	<input type="hidden" name="submit_status" value="repassword">
+                    	<input type="hidden" name="id" value="${id }">
+                    	<input type="hidden" id="password" name="password" value="${password }">
+                        <div class="navigation_wrap">
+                            <progress class="bar_navigation" value="60" max="100"></progress>
+                        </div>
+                        <h2>카카오계정 가입을 위해<br>
+                            비밀번호를 확인해 주세요.</h2>
+                        <div class="item_tf">
+                            <input type="password" class="item_ip" name="repassword" placeholder="비밀번호 확인" autofocus="autofocus">
+                        </div>
+                        <div class="item_msg">
+                        	<span class="msg1">필수 항목입니다.</span>
+                        	<span class="msg2">비밀번호가 일치하지 않습니다.</span>
+                        </div>
+                        <div class="confirm_btn">
+                            <button type="button" class="btn_g">다음</button>
+                        </div>
+                    </form>
                 </div>
             </main>
             <jsp:include page="../include/sign_up_include/sign_up_footer.jsp"></jsp:include>
--- kakao_spring_junil/src/main/webapp/static/js/sign_up.js
+++ kakao_spring_junil/src/main/webapp/static/js/sign_up.js
@@ -4,6 +4,15 @@ warp_form[0].style.display = 'block';
 const item_ip = document.querySelectorAll('.item_ip');
 const btn_g = document.querySelectorAll('.btn_g');
 
+var signUpData = {
+	signUpEmail: '',
+	emailFlag: 0,
+	signUpPassword: '',
+	signUpName: '',
+	signUpPhone: '',
+	phoneFlag: 0
+}
+
 item_ip[0].onkeypress = () => {
 	if(window.event.keyCode == 13){
 		window.event.preventDefault();
@@ -16,11 +25,12 @@ btn_g[0].onclick = () => {
 }
 
 function emailNext(indexNUmber) {
+	const msg1 = document.querySelectorAll('.msg1');
+	const msg2 = document.querySelectorAll('.msg2');
+	msg1[indexNUmber].style.display = 'none';
+	msg2[indexNUmber].style.display = 'none';
 	if(item_ip[indexNUmber].value.length == 0){
-		const msg1 = document.querySelectorAll('.msg1');
-		const msg2 = document.querySelectorAll('.msg2');
 		msg1[indexNUmber].style.display = 'block';
-		msg2[indexNUmber].style.display = 'none';
 	} else {
 		$.ajax({
 			type: "post",
@@ -30,11 +40,89 @@ function emailNext(indexNUmber) {
 			},
 			dataType: "text",
 			success: function(data){
-				
+				signUpData = JSON.parse(data);
+				if(signUpData.emailFlag == 0){
+					warp_form[indexNUmber].style.display = 'none';
+					warp_form[indexNUmber+1].style.display = 'block';
+				}else if(signUpData.emailFlag == 1){
+					const false_email = document.querySelector('#false_email');
+					while(false_email.hasChildNodes()){
+						false_email.removeChild(false_email.firstChild);
+					}
+					const emailTextNode = document.createTextNode(signUpData.signUpEmail);
+					false_email.appendChild(emailTextNode);
+					msg2[indexNUmber].style.display = 'block';
+				}
 			},
 			error:function(){
-				
+				alert('비동기 처리 오류!');
 			}
 		})
+	}
+}
+
+item_ip[1].onkeypress = () => {
+	if(window.event.keyCode == 13){
+		window.event.preventDefault();
+		passwordNext(1);
+	}
+}
+
+btn_g[1].onclick = () => {
+	passwordNext(1);
+}
+
+function checkPassword(id, password, msg2){
+	while(msg2.hasChildNodes()){
+		msg2.removeChild(msg2.firstChild);
+	}
+	
+	
+    if(!/^[a-zA-Z0-9]{10,15}$/.test(password)){
+        let msg = document.createTextNode('숫자와 영문자 조합으로 10~15자리를 사용해야 합니다.');
+        msg2.appendChild(msg);
+        return false;
+    }
+    
+    var checkNumber = password.search(/[0-9]/g);
+    var checkEnglish = password.search(/[a-z]/ig);
+    
+    if(checkNumber <0 || checkEnglish <0){
+        let msg = document.createTextNode("숫자와 영문자를 혼용하여야 합니다.");
+        msg2.appendChild(msg);
+        return false;
+    }
+    
+    if(/(\w)\1\1\1/.test(password)){
+        let msg = document.createTextNode('444같은 문자를 4번 이상 사용하실 수 없습니다.');
+        msg2.appendChild(msg);
+        return false;
+    }
+    
+    if(password.search(id) > -1){
+        let msg = document.createTextNode("비밀번호에 아이디가 포함되었습니다.");
+        msg2.appendChild(msg);
+        return false;
+    }
+    
+    
+    return true;
+    
+}
+
+function passwordNext(indexNUmber) {
+	const msg1 = document.querySelectorAll('.msg1');
+	const msg2 = document.querySelectorAll('.msg2');
+	msg1[indexNUmber].style.display = 'none';
+	msg2[indexNUmber].style.display = 'none';
+	if(item_ip[indexNUmber].value.length == 0){
+		msg1[indexNUmber].style.display = 'block';
+	} else {
+		let checkFlag = checkPassword(signUpData.signUpEmail, item_ip[indexNUmber].value, msg2[indexNUmber]);
+		if(checkFlag == true) {
+			
+		}else {
+			msg2[indexNUmber].style.display = 'block';
+		}
 	}
 }
